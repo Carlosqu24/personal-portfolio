ef4ee12c21387fd5d79a1aff759e460b7936b3d3
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-04-10 22:11:29 -0600

    Projects displayed on UI removed

--- projects.js
+++ projects.js
@@ -1,98 +1,111 @@
-export default [{
-    id: 1,
-    imageUrl: './assets/images/projects/architecture-landing-page.png',
-    name: 'Architecture page',
-    category: "Landing page",
-    description: "It's an example landing page about an architecture agency. It's divided by 4 sections: Hero, About, Services and Projects.",
-    reposLinks: {
-        productionUrl: "https://carlosqu24.github.io/react-architecture-landing-page/",
-        githubUrl: "https://github.com/Carlosqu24/react-architecture-landing-page"
-    }
-}, {
+export default [
+  //     {
+  //     id: 1,
+  //     imageUrl: './assets/images/projects/architecture-landing-page.png',
+  //     name: 'Architecture page',
+  //     category: "Landing page",
+  //     description: "It's an example landing page about an architecture agency. It's divided by 4 sections: Hero, About, Services and Projects.",
+  //     reposLinks: {
+  //         productionUrl: "https://carlosqu24.github.io/react-architecture-landing-page/",
+  //         githubUrl: "https://github.com/Carlosqu24/react-architecture-landing-page"
+  //     }
+  // },
+  {
     id: 2,
-    imageUrl: './assets/images/projects/js-contacts-app-1.png',
-    name: 'Contacts app',
+    imageUrl: "./assets/images/projects/js-contacts-app-1.png",
+    name: "Contacts app",
     category: "Web application",
-    description: "It's an application to manage your contacts. You can upload an image and save it that refers to the contact. You can view, add, edit and delete your personalizated contacts. I developed it with HTML, CSS and Javascript. This application has data persistence, which is the Local Storage.",
+    description:
+      "It's an application to manage your contacts. You can upload an image and save it that refers to the contact. You can view, add, edit and delete your personalizated contacts. I developed it with HTML, CSS and Javascript. This application has data persistence, which is the Local Storage.",
     reposLinks: {
-        productionUrl: "https://carlosqu24.github.io/js-contacts-app/",
-        githubUrl: "https://github.com/Carlosqu24/js-contacts-app"
-    }
-}, {
+      productionUrl: "https://carlosqu24.github.io/js-contacts-app/",
+      githubUrl: "https://github.com/Carlosqu24/js-contacts-app",
+    },
+  },
+  {
     id: 3,
-    imageUrl: './assets/images/projects/react-context-tasks.png',
-    name: 'Tasks CRUD',
+    imageUrl: "./assets/images/projects/react-context-tasks.png",
+    name: "Tasks CRUD",
     category: "Web application",
-    description: "I am currently working on an application for task management. You can view, add, edit and delete tasks. I developed it with React, the Javascript library. This application has data persistence, which is the Local Storage.",
+    description:
+      "I am currently working on an application for task management. You can view, add, edit and delete tasks. I developed it with React, the Javascript library. This application has data persistence, which is the Local Storage.",
     reposLinks: {
-        productionUrl: "https://carlosqu24.github.io/react-context-tasks-app/#/",
-        githubUrl: "https://github.com/Carlosqu24/react-context-tasks-app"
-    }
-}, {
+      productionUrl: "https://carlosqu24.github.io/react-context-tasks-app/#/",
+      githubUrl: "https://github.com/Carlosqu24/react-context-tasks-app",
+    },
+  },
+  {
     id: 4,
-    imageUrl: './assets/images/projects/countdown-min.png',
-    name: 'Countdown',
+    imageUrl: "./assets/images/projects/countdown-min.png",
+    name: "Countdown",
     category: "Web application",
-    description: "It is a project that shows the countdown of the date preset by the user with respect to the current time.",
+    description:
+      "It is a project that shows the countdown of the date preset by the user with respect to the current time.",
     reposLinks: {
-        productionUrl: "https://carlosqu24.github.io/js-countdown/",
-        githubUrl: "https://github.com/Carlosqu24/js-countdown"
-    }
-}, 
-// {
-//     id: 5,
-//     imageUrl: './assets/images/projects/filter-images-gallery-min.png',
-//     name: 'Filter images gallery',
-//     category: "Web application",
-//     description: "It is a project that filters images according to their name. These images are brought from the Pixabay API.",
-//     reposLinks: {
-//         productionUrl: "https://carlosqu24.github.io/js-gallery-filter-images/",
-//         githubUrl: "https://github.com/Carlosqu24/js-gallery-filter-images"
-//     }
-// }
-, {
+      productionUrl: "https://carlosqu24.github.io/js-countdown/",
+      githubUrl: "https://github.com/Carlosqu24/js-countdown",
+    },
+  },
+  ,
+  // {
+  //     id: 5,
+  //     imageUrl: './assets/images/projects/filter-images-gallery-min.png',
+  //     name: 'Filter images gallery',
+  //     category: "Web application",
+  //     description: "It is a project that filters images according to their name. These images are brought from the Pixabay API.",
+  //     reposLinks: {
+  //         productionUrl: "https://carlosqu24.github.io/js-gallery-filter-images/",
+  //         githubUrl: "https://github.com/Carlosqu24/js-gallery-filter-images"
+  //     }
+  // }
+  {
     id: 6,
-    imageUrl: './assets/images/projects/budget-app.png',
-    name: 'Budget App',
+    imageUrl: "./assets/images/projects/budget-app.png",
+    name: "Budget App",
     category: "Web application",
-    description: "It is an application about managing personal expenses. It is divided into 3 sections: one to show all expenses, the second to add new ones and the third to set a default amount in the budget.",
+    description:
+      "It is an application about managing personal expenses. It is divided into 3 sections: one to show all expenses, the second to add new ones and the third to set a default amount in the budget.",
     reposLinks: {
-        productionUrl: "https://carlosqu24.github.io/react-budget-app/",
-        githubUrl: "https://github.com/Carlosqu24/react-budget-app"
-    }
-}, {
+      productionUrl: "https://carlosqu24.github.io/react-budget-app/",
+      githubUrl: "https://github.com/Carlosqu24/react-budget-app",
+    },
+  },
+  {
     id: 7,
-    imageUrl: './assets/images/projects/react-pizza-landing-page-image-url.PNG',
-    name: 'Pizzeria landing page example',
+    imageUrl: "./assets/images/projects/react-pizza-landing-page-image-url.PNG",
+    name: "Pizzeria landing page example",
     category: "Landing page",
     description: "It's an example landing page about a pizza restaurant",
     reposLinks: {
-        productionUrl: "https://carlosqu24.github.io/react-pizza-landing-page/",
-        githubUrl: "https://github.com/Carlosqu24/react-budget-app"
-    }
-}, {
-    id: 8,
-    imageUrl: './assets/images/projects/coffee-shop-example.PNG',
-    name: 'Coffee shop landing page example',
-    category: "Landing page",
-    description: "It's an example landing page about a coffee shop",
-    reposLinks: {
-        productionUrl: "https://carlosqu24.github.io/coffee-shop-simple-landing-page/",
-        githubUrl: "https://github.com/Carlosqu24/react-budget-app"
-    }
-}, {
+      productionUrl: "https://carlosqu24.github.io/react-pizza-landing-page/",
+      githubUrl: "https://github.com/Carlosqu24/react-budget-app",
+    },
+  },
+  //  {
+  //     id: 8,
+  //     imageUrl: './assets/images/projects/coffee-shop-example.PNG',
+  //     name: 'Coffee shop landing page example',
+  //     category: "Landing page",
+  //     description: "It's an example landing page about a coffee shop",
+  //     reposLinks: {
+  //         productionUrl: "https://carlosqu24.github.io/coffee-shop-simple-landing-page/",
+  //         githubUrl: "https://github.com/Carlosqu24/react-budget-app"
+  //     }
+  // },
+  {
     id: 9,
-    imageUrl: './assets/images/projects/travl.png',
-    name: 'Travel agency landing page example',
+    imageUrl: "./assets/images/projects/travl.png",
+    name: "Travel agency landing page example",
     category: "Landing page",
     description: "It's an example landing page about a travel agency",
     reposLinks: {
-        productionUrl: "https://carlosqu24.github.io/travl-landing-page/",
-        githubUrl: "https://github.com/Carlosqu24/react-budget-app"
-    }
-}]
+      productionUrl: "https://carlosqu24.github.io/travl-landing-page/",
+      githubUrl: "https://github.com/Carlosqu24/react-budget-app",
+    },
+  },
+];
 
 export const PROJECTS_CATEGORIES_NAMES = {
-    LANDING_PAGE: 'Landing page',
-    WEB_APPLICATION: 'Web application'
-}
+  LANDING_PAGE: "Landing page",
+  WEB_APPLICATION: "Web application",
+};
